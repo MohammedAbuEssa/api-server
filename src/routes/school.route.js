7419b1d3b577c88ae63559fbb17933b9cb28a518
MohammedAbuEssa <mabuissa5@gmail.com>
2023-06-14 14:37:45 +0000

    Lab: Class 04

--- src/routes/school.route.js
+++ src/routes/school.route.js
@@ -1,45 +1,47 @@
 const express = require("express");
 const schoolRouter = express.Router();
-const { School } = require("../models/index");
+const { PersonModal, SchoolModal } = require("../models/index");
+const Collection = require("../models/lib/collection");
 
-schoolRouter.get("/school", getAllSchool);
+schoolRouter.get("/school", getSchool);
 schoolRouter.get("/school/:id", getSchool);
 schoolRouter.post("/school", createSchool);
 schoolRouter.put("/school/:id", updateSchool);
 schoolRouter.delete("/school/:id", deleteSchool);
+schoolRouter.get("/schoolperson/:id", getSchoolPerson);
+// async function getAllSchool(req, res) {
+//   let sechoolResult = await School.findAll();
+//   res.status(200).json(sechoolResult);
+// }
 
-async function getAllSchool(req, res) {
-  let sechoolResult = await School.findAll();
-  res.status(200).json(sechoolResult);
+async function getSchoolPerson(req, res) {
+  let { id } = req.params;
+  let schoolResult = await SchoolModal.readSchoolPerson(id, PersonModal.model);
+  res.status(200).send(schoolResult);
 }
 
 async function getSchool(req, res) {
   const schoolId = parseInt(req.params.id);
-  let school = await School.findOne({
-    where: {
-      id: schoolId,
-    },
-  });
+  let school = await SchoolModal.read(schoolId);
   res.status(200).json(school);
 }
 
 async function createSchool(req, res) {
   let newSchool = req.body;
-  let school = await School.create(newSchool);
+  let school = await SchoolModal.add(newSchool);
   res.status(201).json(school);
 }
 
 async function updateSchool(req, res) {
   let schoolId = parseInt(req.params.id);
   let updateSchool = req.body;
-  let foundSchool = await School.findOne({ where: { id: schoolId } });
-  let updatedSchool = await foundSchool.update(updateSchool);
+  let updatedSchool = await SchoolModal.update(updateSchool, schoolId);
   res.status(201).json(updatedSchool);
 }
 
 async function deleteSchool(req, res) {
   let schoolId = parseInt(req.params.id);
-  let deleteSchool = await School.destroy({ where: { id: schoolId } });
+  let deleteSchool = await SchoolModal.delete(schoolId);
   res.status(204).json(deleteSchool);
 }
 
